b41fe39f114514a3bd86ac9de418abb927cda1ce
Joseph Lee <jo112095@gmail.com>
2021-05-20 16:31:20 -0400

    tutorial 8 code working

--- play_go_web_app/play_go_web_app/settings.py
+++ play_go_web_app/play_go_web_app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
