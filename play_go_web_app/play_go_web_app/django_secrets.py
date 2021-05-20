a37a5ff2274ceafae9e4db419fb56c0642625ed6
Joseph Lee <jo112095@gmail.com>
2021-05-20 15:37:18 -0400

    protected secret key successfully

---
+++ play_go_web_app/play_go_web_app/django_secrets.py
@@ -0,0 +1,9 @@
+SECRET_CODE = 'django-insecure-d6v(q#%#57y6(no*p#^!bt6ga2&rswtxx)aeztmu%vjqs&-*do'
+
+
+def main():
+    print("Test")
+
+
+if __name__ == "__main__":
+    main()
--- play_go_web_app/play_go_web_app/settings.py
+++ play_go_web_app/play_go_web_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .django_secrets import SECRET_CODE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.SECRET_KEY
+SECRET_KEY = SECRET_CODE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
