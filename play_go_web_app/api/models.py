c3440b5032f75097989011be0767ffbcb5d84c70
Joseph Lee <jo112095@gmail.com>
2021-05-21 19:33:38 -0400

    fixed indentation

--- play_go_web_app/api/models.py
+++ play_go_web_app/api/models.py
@@ -21,11 +21,9 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-# state i.e. 2d array to store the go board
-# we must use a charfield which then must be converted
-# from javascript -> string -> python
-# and then,
-# from python -> string -> javascript
-board = models.CharField(max_length=765, unique=False, default="[]")
+    # state i.e. 2d array to store the go board
+    # we must use a charfield which then must be converted
+    # from javascript -> string -> python
+    # and then,
+    # from python -> string -> javascript
+    board = models.CharField(max_length=765, unique=False, default="[]")
