d01a3a99d87c6ce452c0512f09a167c12f41c33d
Joseph Lee <jo112095@gmail.com>
2021-05-21 19:48:19 -0400

    figured outv that changes to Django model --> a new migration Python file

--- play_go_web_app/api/models.py
+++ play_go_web_app/api/models.py
@@ -21,6 +21,7 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+
     # state i.e. 2d array to store the go board
     # we must use a charfield which then must be converted
     # from javascript -> string -> python
