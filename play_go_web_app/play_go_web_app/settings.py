5775702dd40f4923aa061f5f7e5e3967d5928860
Joseph S. Lee <jslee08@email.wm.edu>
2022-01-02 23:21:56 -0500

    Add local host to allowed hosts

--- play_go_web_app/play_go_web_app/settings.py
+++ play_go_web_app/play_go_web_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_CODE
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [SECRET_HOST]
+ALLOWED_HOSTS = ['localhost', SECRET_HOST]
 
 
 # Application definition
