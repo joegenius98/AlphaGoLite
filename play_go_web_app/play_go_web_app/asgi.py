39cba239c8dec97d92a21b6efb831e1d09d9e362
Joseph Lee <jo112095@gmail.com>
2022-05-29 20:11:58 -0700

    Fix import order in `asgi.py`

--- play_go_web_app/play_go_web_app/asgi.py
+++ play_go_web_app/play_go_web_app/asgi.py
@@ -1,14 +1,17 @@
+from django.core.asgi import get_asgi_application
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "play_go_web_app.settings")
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from api.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "play_go_web_app.settings")
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
